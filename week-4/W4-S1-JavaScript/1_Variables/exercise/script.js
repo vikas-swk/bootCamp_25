1e2c43d613bf012112ff311bb542b0b250a4992d
Vikas <vikas_swk@yahoo.com>
2025-01-27 12:08:02 +0000

    week-3 and week-4 files

---
+++ week-4/W4-S1-JavaScript/1_Variables/exercise/script.js
@@ -0,0 +1,20 @@
+var firstName = "John";
+var surName = "Smith";
+
+const surName = "Doe";
+surName = "Doe";
+
+var age = 25;
+var retirementAge = 67;
+
+console.log(firstName + " " + surName);
+
+console.log(`hello ${firstName} `);
+
+// TODO: log out the full name "John Smith" by concatenating the variables firstName and surName
+
+// TODO: log out "John Smith and I amd 25 years old"
+
+// TODO: Create a variable to store the number of years until retirement
+
+// TODO: log out "I have 42 years until retirement"
---
+++ week-4/W4-S3-TheDOM/10_EventHandling/exercise/assets/js/script.js
@@ -0,0 +1,13 @@
+// 1. use document.getElementById to select the searchTerm Button
+
+// 2. use document.getElementById to select the searchButton Button
+
+// 3. add an event listener to the searchButton that calls the search function when clicked
+
+function search() {
+  // 4. use the value property of the searchInput to get the search term
+  // 5. select the searches div using document.getElementById
+  // 6. create a new li element using document.createElement
+  // 7. set the innerHTML of the new paragraph to the search term
+  // 8. append the new paragraph to the searches div
+}
