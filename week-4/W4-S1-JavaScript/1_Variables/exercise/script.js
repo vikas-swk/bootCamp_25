8898a4aae8efdd30f601e0aedd9baf64f48c9841
Vikas <vikas_swk@yahoo.com>
2025-01-27 17:42:10 +0000

    Completed exercise on JS

--- week-4/W4-S1-JavaScript/1_Variables/exercise/script.js
+++ week-4/W4-S1-JavaScript/1_Variables/exercise/script.js
@@ -1,20 +1,41 @@
 var firstName = "John";
 var surName = "Smith";
 
-const surName = "Doe";
-surName = "Doe";
+//const surName = "Doe";
+//surName = "Doe";
 
 var age = 25;
 var retirementAge = 67;
 
 console.log(firstName + " " + surName);
 
-console.log(`hello ${firstName} `);
+console.log(`Hello ${firstName}`);
+
+console.log(firstName + " " +surName + "and I am" + age + "years old");
+
+console.log(firstName + " " +surName + "and I am" + age + "years old");
+
+var a = 12; // stored 12 in a
+var b = 5; // stored 5 in b
+
+sum = a + b; // sum of a and b is stored in sum
+console.log(sum); //logging in console
+
+const c = 'Apple '; // stored Apple in c
+const d = 'Orange'; //stored Orange in d
+const e = 'Fruits'; //stored Fruits in e
+
+console.log('These are' + ' ' + e + ' ' + c + '&' + ' ' + d) //logging in console and concatenating the strings
+
 
 // TODO: log out the full name "John Smith" by concatenating the variables firstName and surName
+/* Solution: console.log(firstName + " " + surName); */
 
 // TODO: log out "John Smith and I amd 25 years old"
+/* Solution console.log(firstName + " " +surName + "and I am" + age + "years old"); */
 
 // TODO: Create a variable to store the number of years until retirement
+/* Solution: var = yearsUntilRetirement = retirementAge - age; */
 
 // TODO: log out "I have 42 years until retirement"
+// Solution: console.log("I have" + retirementAge - age + "years unitl retirement");
